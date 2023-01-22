4bbb8090409c41f76f682c7d8adc3da552023372
Abdessamade-OH <abdessamade.ohmad@etu.uae.ac.ma>
2023-01-22 17:53:50 +0100

    tables des categories et produits ajoutés

---
+++ database/migrations/2023_01_22_162249_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->string('icon_class'); //au cas où on travaille pas avec les images
+                                          //sinon on peut effacer ce colonne.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
